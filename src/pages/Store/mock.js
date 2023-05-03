c5f63b019db513cd940a5519101d8a7289f7f274
Daniel Dias <daniiel2dias@gmail.com>
2023-05-03 18:07:22 -0300

    mock altered

--- src/pages/Store/mock.js
+++ src/pages/Store/mock.js
@@ -1,5 +1,3 @@
-
-
 const PRODUTOS = [
   { id: 1, nome: "Garrafa esportiva 500ML preta", preco: "R$70,00", imagem: "https://bit.ly/42fcCqy" },
   { id: 2, nome: "Corlar em prata e topázio", preco: "R$169,90", imagem: "https://bit.ly/424Lr2c" },
@@ -7,12 +5,15 @@ const PRODUTOS = [
   { id: 4, nome: "Anel de prata com topázio", preco: "R$60,00", imagem: "https://bit.ly/44muMIM" } ,
   { id: 5, nome: "Short preto Wilson", preco: "R$80,00", imagem: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTwkCo0Dmgb7OTEfGgqsazj2ZQoeZIz4nJK5oXC2z1iUuMiLo4UjetiHPQMIJQMlGrAgycqbWLv99j36iiB6TJguNsUv8_OZHUukmoAztlbwcd7K4SkCT-x7sUC&usqp=CAE" },
   { id: 7, nome: "Moletom preto da Akatsuki", preco: "R$149,90", imagem: "https://bit.ly/3NumjNN" },
-  { id: 8, nome: "Garrafa preta personalizada", preco: "R$89,90", imagem: "https://bitlybr.com/kmFYN" },
-  { id: 9, nome: "Blusa social masculina cinza", preco: "R$120,00", imagem: "https://bit.ly/3p1OsBE" },
-  { id: 10, nome: "Garrafa preta personalizada", preco: "R$65,80", imagem: "https://bit.ly/3noe0IF"},
+  { id: 8, nome: "Saia feminina colegial xadre", preco: "R$89,90", imagem: "https://encurtador.com.br/xIO23" },
+  { id: 9, nome: "Blusa social masculina cinza", preco: "R$120,00", imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNsiCgw_EO8nq0_cJWoJoImPpiFABYak-M2w&usqp=CAU" },
+  { id: 10, nome: "Relógio Wegger", preco: "R$65,80", imagem: "https://encurtador.com.br/dlwX6"},
   { id: 11, nome: "Calça rasgada masculina preta", preco: "R$90,00", imagem: "https://bit.ly/3LNNu4J"},
   { id: 12, nome: "Tênis All Star clássico", preco: "R$200,00", imagem: "https://bit.ly/3LMtVKi"},
-  { id: 13, nome: "Jaqueta Jeans estilizada ", preco: "R$189,90", imagem: "https://bit.ly/3AORgoi"},
+  { id: 14, nome: "Blusa jeans bege masculina ", preco: "R$189,90", imagem: "https://armadillo.vteximg.com.br/arquivos/ids/175114/camisa-pipa-argila--2-.jpg?v=638048910747330000"},
+  { id: 15, nome: "Sapato alto preto feminino ", preco: "R$189,90", imagem: "https://encurtador.com.br/deklP"},
+  { id: 17, nome: "Blusa branca com preto masculina ", preco: "R$189,90", imagem: "https://i.pinimg.com/550x/6a/a4/7a/6aa47a003324dc6469f0371d31ba9e5f.jpg"},
+  { id: 18, nome: "Calça preta feminina ", preco: "R$189,90", imagem: "https://encurtador.com.br/evGOR"},
 ]
 
 export default PRODUTOS;
