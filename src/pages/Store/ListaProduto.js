d9d9cf84f56c86c58c2861aac95464697c056a4d
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-03 13:16:17 -0300

    mudanças no layout da store

--- src/pages/Store/ListaProduto.js
+++ src/pages/Store/ListaProduto.js
@@ -1,22 +1,55 @@
-import MyButton from "../../components/Button";
 import CardProduto from "./CardProduto";
-import styled from "styled-components";
+import styled, { keyframes } from 'styled-components';
 import { useNavigate } from "react-router-dom";
+import CarrinhoFundoPreto from "./img/cart1-svgrepo-com (1).svg"
+import user from "./img/user-svgrepo-com (2).svg"
+import { useState } from "react";
+import PRODUTOS from "./mock";
 
-export default function ListaProduto({ produtos, adicionarAoCarrinho }) {
+
+
+
+const MyButton = ({ Text, onClick, Type = "button" }) => {
+  return (
+    <Button type={Type} onClick={onClick}>
+      {Text}
+    </Button>
+  );
+};
+
+
+export default function ListaProduto({ adicionarAoCarrinho }) {
   const navigate = useNavigate();
+  const [busca, setBusca] = useState("");
+
+  const produtosFiltrados = PRODUTOS.filter((produtoPesquisado) =>
+    produtoPesquisado.nome.toLowerCase().startsWith(busca.toLowerCase())
+  );
 
   return (
     <PageStore>
-      <Logo>BuyMarket</Logo>
-      <Center>
-        <MyButton
-          Text="Cadastrar Produto"
-          onClick={() => navigate("/cadastro-produto")}
+      <Logo>
+        <h1> BuyMarket </h1>
+        <Input
+          type=""
+          value={busca}
+          onChange={(ev) => setBusca(ev.target.value)}
+        
         />
-      </Center>
+
+        <Aviso>
+          <p>Frete grátis </p>
+          <MyButton
+            Text="Cadastre seu produto"
+            onClick={() => navigate("cadastro-produto")}
+
+          />
+          <img src={user} />
+          <img src={CarrinhoFundoPreto} />
+        </Aviso>
+      </Logo>
       <ContainerListaProdutos>
-        {produtos.map((prod) => (
+        {produtosFiltrados.map((prod) => (
           <CardProduto
             key={prod.id}
             produto={prod}
@@ -29,34 +62,46 @@ export default function ListaProduto({ produtos, adicionarAoCarrinho }) {
 }
 
 const Logo = styled.div`
-  background: linear-gradient(
-    to right,
-    #0d5c92,
-    #4682b4
-  ); /* alterando as cores para uma tonalidade mais azulada */
-  color: white;
-  padding: 20px;
-  text-align: center;
-  text-shadow: 2px 2px #333;
-  font-size: 3rem;
-  font-family: "Arial Black", sans-serif;
-  width: 100vw;
+  background: radial-gradient(circle, #393939 0%, #000000 100%);
+  background: linear-gradient(to right, #0d5c92, #4682b4);
+  box-shadow: inset 0 0 10px rgba(255, 255, 255, 0.2), 0 0 20px rgba(255, 255, 255, 0.2);
+  height:100px;
+  display:flex;
+  align-items: center;
 
-  img {
-    width: 50px;
+
+  h1 {
+    color: white;
+    font-size: 2.5rem;
+    font-family: "Arial Black", sans-serif;
+    letter-spacing: 2px;
+    padding: 20px;
+    text-shadow: 2px 2px #333;
+    width: 100vw;
+    margin-left:10px;
+    flex:1;
+
   }
+  img {
+            width: 40px;
+            color:white;
+            flex:1;
+            text-align:center;
+          }
 `;
 
 const PageStore = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  width: 80vw;
+  width: 100vw;
   height: 100vh;
 `;
 const ContainerListaProdutos = styled.div`
   display: flex;
   flex-wrap: wrap;
+  margin-left:80px;
+  margin-top:20px;
 
   img {
     width:150px;
@@ -64,9 +109,62 @@ const ContainerListaProdutos = styled.div`
   }
 `;
 
-const Center = styled.div`
-  display: flex;
-  justify-content: center;
-`
 
 
+const Button = styled.button`
+  padding: 16px 20px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  cursor: pointer;
+  background-color: transparent;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  max-width: 200px;
+  margin: 3px;
+
+  &:active {
+    transform: scale(0.97);
+  }
+`;
+
+const Input = styled.input`
+height: 48px;
+border: 1px solid #EDEDED;
+border-radius: 8px 0px 0px 0px;
+padding: 8px 16px;
+font-family: "Roboto",sans-serif;
+font-size: 14px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-right:100px;
+width: 500px;
+outline: none;
+
+`;
+
+
+const Aviso = styled.div`
+color:red;
+margin-right:10px;
+display:flex;
+justify-content:space-between;
+align-items: center;
+p {
+  animation: ${keyframes`
+  0% {
+    color:red;
+  }
+  50% {
+    color:#ddd;
+  }
+  100% {
+    color:red;
+  }
+  `
+  } 2s linear infinite;
+}
+`;
--- src/pages/Store/mock.js
+++ src/pages/Store/mock.js
@@ -1,16 +1,19 @@
-const PRODUTOS = [
-    { id: 1, nome: "Camiseta", preco: "R$50,00", imagem: "https://cdn.awsli.com.br/600x450/238/238483/produto/40016383/7337aa5991.jpg" },
-    { id: 2, nome: "Calça", preco: "R$150,00", imagem: "https://www.invictus.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/l/e/legion-azul-frente.jpg" },
-    { id: 3, nome: "Vestido", preco: "R$70,00", imagem: "https://cdn.awsli.com.br/600x450/238/238483/produto/40016383/7337aa5991.jpg"},
-    { id: 4, nome: "Saia", preco: "R$60,00", imagem: "https://cdn.awsli.com.br/600x450/238/238483/produto/40016383/7337aa5991.jpg" } ,
-    { id: 5, nome: "Bermuda", preco: "R$40,00", imagem: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTwkCo0Dmgb7OTEfGgqsazj2ZQoeZIz4nJK5oXC2z1iUuMiLo4UjetiHPQMIJQMlGrAgycqbWLv99j36iiB6TJguNsUv8_OZHUukmoAztlbwcd7K4SkCT-x7sUC&usqp=CAE" },
-    { id: 7, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-    { id: 8, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-    { id: 9, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-    { id: 10, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-    { id: 11, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-    { id: 12, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-    { id: 13, nome: "Shorts", preco: "R$60,00", imagem: "https://cdn-images.farfetch-contents.com/14/16/01/84/14160184_18904041_300.jpg" },
-  ]
 
-  export default PRODUTOS;
+
+const PRODUTOS = [
+  { id: 1, nome: "Garrafa esportiva 500ML preta", preco: "R$70,00", imagem: "https://bit.ly/42fcCqy" },
+  { id: 2, nome: "Corlar em prata e topázio", preco: "R$169,90", imagem: "https://bit.ly/424Lr2c" },
+  { id: 3, nome: "Camisa vermelha, tamanho ÚNICO", preco: "R$70,00", imagem: "https://cdn.awsli.com.br/600x450/238/238483/produto/40016383/7337aa5991.jpg"},
+  { id: 4, nome: "Anel de prata com topázio", preco: "R$60,00", imagem: "https://bit.ly/44muMIM" } ,
+  { id: 5, nome: "Short preto Wilson", preco: "R$80,00", imagem: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTwkCo0Dmgb7OTEfGgqsazj2ZQoeZIz4nJK5oXC2z1iUuMiLo4UjetiHPQMIJQMlGrAgycqbWLv99j36iiB6TJguNsUv8_OZHUukmoAztlbwcd7K4SkCT-x7sUC&usqp=CAE" },
+  { id: 7, nome: "Moletom preto da Akatsuki", preco: "R$149,90", imagem: "https://bit.ly/3NumjNN" },
+  { id: 8, nome: "Garrafa preta personalizada", preco: "R$89,90", imagem: "https://bitlybr.com/kmFYN" },
+  { id: 9, nome: "Blusa social masculina cinza", preco: "R$120,00", imagem: "https://bit.ly/3p1OsBE" },
+  { id: 10, nome: "Garrafa preta personalizada", preco: "R$65,80", imagem: "https://bit.ly/3noe0IF"},
+  { id: 11, nome: "Calça rasgada masculina preta", preco: "R$90,00", imagem: "https://bit.ly/3LNNu4J"},
+  { id: 12, nome: "Tênis All Star clássico", preco: "R$200,00", imagem: "https://bit.ly/3LMtVKi"},
+  { id: 13, nome: "Jaqueta Jeans estilizada ", preco: "R$189,90", imagem: "https://bit.ly/3AORgoi"},
+]
+
+export default PRODUTOS;
+
